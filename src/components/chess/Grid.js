85883b603b58d708fa2e49c2b935b40073c6c96d
zqnik <zqnikki@hotmail.com>
2022-05-15 18:41:41 +0800

    pretty code

--- src/components/chess/Grid.js
+++ src/components/chess/Grid.js
@@ -6,74 +6,74 @@ import { WhiteRook, BlackRook } from "./piece/Rook";
 import { WhitePawn, BlackPawn } from "./piece/Pawn";
 
 const GIRD_TYPE = {
-	CORNER: { className: "grid" },
-	FIRST_ROW: { className: "grid first-row" },
-	FIRST_COLUMN: { className: "grid first-column" },
-	CHESS_GIRD: {
-		odd: { className: "chessGird odd" },
-		even: { className: "chessGird even" },
-	},
+  CORNER: { className: "grid" },
+  FIRST_ROW: { className: "grid first-row" },
+  FIRST_COLUMN: { className: "grid first-column" },
+  CHESS_GIRD: {
+    odd: { className: "chessGird odd" },
+    even: { className: "chessGird even" },
+  },
 };
 
 const Grid = ({ girdType, text, x, y, chessType }) => {
-	let chess;
-	switch (chessType) {
-		case "WhiteKing":
-			chess = <WhiteKing />;
-			break;
-		case "BlackKing":
-			chess = <BlackKing />;
-			break;
-		case "WhiteQueen":
-			chess = <WhiteQueen />;
-			break;
-		case "BlackQueen":
-			chess = <BlackQueen />;
-			break;
-		case "WhiteBishop":
-			chess = <WhiteBishop />;
-			break;
-		case "BlackBishop":
-			chess = <BlackBishop />;
-			break;
-		case "WhiteKnight":
-			chess = <WhiteKnight />;
-			break;
-		case "BlackKnight":
-			chess = <BlackKnight />;
-			break;
-		case "WhiteRook":
-			chess = <WhiteRook />;
-			break;
-		case "BlackRook":
-			chess = <BlackRook />;
-			break;
-		case "WhitePawn":
-			chess = <WhitePawn />;
-			break;
-		case "BlackPawn":
-			chess = <BlackPawn />;
-	}
+  let chess;
+  switch (chessType) {
+    case "WhiteKing":
+      chess = <WhiteKing />;
+      break;
+    case "BlackKing":
+      chess = <BlackKing />;
+      break;
+    case "WhiteQueen":
+      chess = <WhiteQueen />;
+      break;
+    case "BlackQueen":
+      chess = <BlackQueen />;
+      break;
+    case "WhiteBishop":
+      chess = <WhiteBishop />;
+      break;
+    case "BlackBishop":
+      chess = <BlackBishop />;
+      break;
+    case "WhiteKnight":
+      chess = <WhiteKnight />;
+      break;
+    case "BlackKnight":
+      chess = <BlackKnight />;
+      break;
+    case "WhiteRook":
+      chess = <WhiteRook />;
+      break;
+    case "BlackRook":
+      chess = <BlackRook />;
+      break;
+    case "WhitePawn":
+      chess = <WhitePawn />;
+      break;
+    case "BlackPawn":
+      chess = <BlackPawn />;
+  }
 
-	const position = {
-		x: x,
-		y: y,
-	};
+  const position = {
+    x: x,
+    y: y,
+  };
 
-	const positionStr =
-		girdType === GIRD_TYPE.CHESS_GIRD.even ||
-			girdType === GIRD_TYPE.CHESS_GIRD.odd
-			? String.fromCharCode(64 + position.x) + position.y
-			: "";
+  const positionStr =
+    girdType === GIRD_TYPE.CHESS_GIRD.even ||
+    girdType === GIRD_TYPE.CHESS_GIRD.odd
+      ? String.fromCharCode(64 + position.x) + position.y
+      : "";
 
-	const girdText = text || "";
-	return (
-		<div className={girdType.className}>
-			{girdText}
-			{chess}
-			<span>{positionStr}</span>
-		</div>
-	);
+  const girdText = text || "";
+  return (
+    <div className={girdType.className}>
+      {girdText}
+      {chess}
+      <span>{positionStr}</span>
+    </div>
+  );
 };
 
 export default Grid;
